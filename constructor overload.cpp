1e5bc56b56aa319d7bdf31c7f9239f2926986654
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-09-02 15:06:17 +0530

    Update constructor overload.cpp

--- constructor overload.cpp
+++ constructor overload.cpp
@@ -6,64 +6,24 @@
 #include <iostream>
 using namespace std;
 
-class add{
-    
-    //int a,b,c,d,e;
-    //float f,g;
-    
+class add {
 public:
+    add(int a, int b) {
+        cout << "The sum of the numbers using add(int a,int b) is: " << a + b << endl;
+    }
 
-add(int a,int b){
-    cout<<"The sum of the numbers using add(int a,int b) is: "<<a+b<<endl;
-}
+    add(int c, int d, int e) {
+        cout << "The sum of the numbers using add(int c,int d,int e) is: " << c + d + e << endl;
+    }
 
-add(int c,int d,int e){
-    cout<<"The sum of the numbers using add(int c,int d,int e) is: "<<c+d+e<<endl;
-}
-
-add(float f, float g){
-    cout<<"The sum of the numbers using add(float f, float g) is: "<<f+g<<endl;
-}
-
+    add(float f, float g) {
+        cout << "The sum of the numbers using add(float f, float g) is: " << f + g << endl;
+    }
 };
 
 int main() {
-    int a,b,c,d,e;
-    float f,g;
-    add x;
-    cout<<"Enter the value for a: ";
-    cin>>a;
-    cout<<endl;
-    cout<<"Enter the value for b: ";
-    cin<<b;
-    cout<<endl;
-    
-    x.add(a,b);
-    cout<<endl;
-    
-    cout<<"Enter the value for c: ";
-    cin<<c;
-    cout<<endl;
-    cout<<"Enter the value for d: ";
-    cin<<d;
-    cout<<endl;
-    cout<<"Enter the value for e: ";
-    cin<<e;
-    cout<<endl;
-    
-    x.add(c,d,e);
-    cout<<endl;
-    
-    cout<<"Enter the value for f: ";
-    cin<<f;
-    cout<<endl;
-    cout<<"Enter the value for g: ";
-    cin<<g;
-    cout<<endl;
-    
-    x.add(f,g);
-    cout<<endl;
-    
+    int a, b, c, d, e;
+    float f, g;
 
-    return 0;
-}
+    cout << "Enter the value for a: ";
+    cin >> a;
