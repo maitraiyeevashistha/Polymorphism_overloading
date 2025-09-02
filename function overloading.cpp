32d2f6c08e4eb5ca05b5b758510e8028ad2c56ef
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-09-02 15:49:58 +0530

    Update function overloading.cpp

--- function overloading.cpp
+++ function overloading.cpp
@@ -1,31 +1,71 @@
+// EXPERIMENT-13
+// MAITRAIYEE VASHISTHA
+// 24070123057
+// ENTC A3
 #include <iostream>
 using namespace std;
 
-class AreaCalculator {
+class Area {
 public:
-    // Circle
+    
     void area(float radius) {
         cout << "Area of Circle (r=" << radius << "): " << 3.14159 * radius * radius << endl;
     }
 
-    // Rectangle
+    
     void area(float length, float width) {
         cout << "Area of Rectangle (" << length << "x" << width << "): " << length * width << endl;
     }
 
-    // Triangle
-    void area(float base, float height, int halfFlag) {
+    
+    void area(float base, float height,int h) {
         cout << "Area of Triangle (b=" << base << ", h=" << height << "): " << 0.5 * base * height << endl;
     }
 };
 
 int main() {
-    AreaCalculator calc;
+    int x,y,z,a,b,c;
+    Area calc;
 
-    // Random creative calls (function overloading in action)
-    calc.area(7.0f);              // Circle with radius
-    calc.area(5.0f, 10.0f);       // Rectangle
-    calc.area(6.0f, 8.0f, 1);     // Triangle
+    cout<<"Enter the radius of circle: ";
+    cin>>x;
+    calc.area(x); 
+    cout<<endl;
+    
+    cout<<"Enter the lenght of rectangle: ";
+    cin>>y;
+    cout<<"Enter the width of rectangle: ";
+    cin>>z;
+    calc.area(y, z);  
+    cout<<endl;
+    
+    cout<<"Enter the height of triangle: ";
+    cin>>a;
+    cout<<"Enter the base of triangle: ";
+    cin>>b;
+    cout<<"Enter the diagonal of triangle: ";
+    cin>>c;
+    calc.area(a,b,c);    
 
     return 0;
 }
+
+/*Output :
+
+Enter the radius of circle: 5
+Area of Circle (r=5): 78.5397
+
+Enter the lenght of rectangle: 6
+Enter the width of rectangle: 8
+Area of Rectangle (6x8): 48
+
+Enter the height of triangle: 5
+Enter the base of triangle: 13
+Enter the diagonal of triangle: 6
+Area of Triangle (b=5, h=13): 32.5
+*/
+
+
+
+
+
